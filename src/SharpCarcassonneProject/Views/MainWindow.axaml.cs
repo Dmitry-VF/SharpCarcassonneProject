e3060c8869acb7532e74aca8856deb3722aba2e4
Dmitry-VF <fomedima@gmail.com>
2022-08-08 23:00:30 +0300

    Initial commit

---
+++ src/SharpCarcassonneProject/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace SharpCarcassonneProject.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
